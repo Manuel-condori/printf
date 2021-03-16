9dfb2f190013218f44b4c54c105bcd61fbcc4187
Manuel-condori <2828@holbertonschool.com>
2021-03-16 15:51:25 +0000

    header

---
+++ holberton.h
@@ -0,0 +1,25 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdarg.h>
+
+/**
+ * struct printer - An structure for each format
+ *
+ * @format: character that represents the format
+ * @func: function for each format
+ */
+typedef struct printer
+{
+	char format;
+	int (*func)(va_list *);
+} printer_t;
+
+int _putchar(char c);
+printer_t get_printer(char s);
+int _printf(const char *format, ...);
+
+
+int print_char(va_list *);
+
+#endif
